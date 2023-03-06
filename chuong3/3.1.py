39cec2d79f49dd4f7af8052008c5a365e3f049fa
tramanh2004 <127073865+tramanh2004@users.noreply.github.com>
2023-03-07 01:01:19 +0700

    Update 3.1.py

--- chuong3/3.1.py
+++ chuong3/3.1.py
@@ -3,8 +3,8 @@ a=float(input('a='))
 b=float(input('b='))
 c=float(input('c='))
 p=(a+b+c)/2
-b=math.sqrt(p*(p-a)*(p-b)*(p-c))
-if ((a+b)>c and (a+c)>b and (b+c)>a) :
-    print('Dientich=',round(b,3))
+s=math.sqrt(p*(p-a)*(p-b)*(p-c))
+if ((a+s)>c and (a+c)>s and (s+c)>a) :
+    print('Dientich=',round(s,3))
 else:
     print('Khong hop le')
